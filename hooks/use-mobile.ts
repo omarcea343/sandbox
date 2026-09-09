03c16888977efc1a798562a28c2e913e800f46ab
Omar Zuniga <miguel_omar30@hotmail.com>
2026-09-08 19:29:48 -0600

    feat: Fix lint issues and format

--- hooks/use-mobile.ts
+++ hooks/use-mobile.ts
@@ -2,18 +2,20 @@ import * as React from "react"
 
 const MOBILE_BREAKPOINT = 768
 
-export function useIsMobile() {
-  const [isMobile, setIsMobile] = React.useState<boolean | undefined>(undefined)
-
-  React.useEffect(() => {
+function subscribe(onStoreChange: () => void) {
     const mql = window.matchMedia(`(max-width: ${MOBILE_BREAKPOINT - 1}px)`)
-    const onChange = () => {
-      setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    }
-    mql.addEventListener("change", onChange)
-    setIsMobile(window.innerWidth < MOBILE_BREAKPOINT)
-    return () => mql.removeEventListener("change", onChange)
-  }, [])
+    mql.addEventListener("change", onStoreChange)
+    return () => mql.removeEventListener("change", onStoreChange)
+}
 
-  return !!isMobile
+function getSnapshot() {
+    return window.innerWidth < MOBILE_BREAKPOINT
+}
+
+function getServerSnapshot() {
+    return false
+}
+
+export function useIsMobile() {
+    return React.useSyncExternalStore(subscribe, getSnapshot, getServerSnapshot)
 }
