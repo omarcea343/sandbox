b28bd79acad9f3719df288afbb8c2a16df50df6a
Omar Zuniga <miguel_omar30@hotmail.com>
2026-09-08 20:53:20 -0600

    feat: add base sidebar

---
+++ components/app-sidebar.tsx
@@ -0,0 +1,11 @@
+import { Sidebar, SidebarContent, SidebarFooter, SidebarHeader } from "@/components/ui/sidebar"
+
+export function AppSidebar({ ...props }: React.ComponentProps<typeof Sidebar>) {
+    return (
+        <Sidebar {...props}>
+            <SidebarHeader />
+            <SidebarContent />
+            <SidebarFooter />
+        </Sidebar>
+    )
+}
